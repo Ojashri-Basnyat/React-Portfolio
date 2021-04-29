b3ac0c88766fc1f1550c43ff0a989e58ab9af6f5
Ojashri Basnyat <basnyat.ojashri@gmail.com>
2021-04-28 21:30:19 -0400

    fixing small errors

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
         <a href="https://github.com/Ojashri-Basnyat"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" class="icon"/></a>
         <a href="https://www.linkedin.com/in/ojashri-basnyat-325703105/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
         <p>
-        &copy; Ojashri Basnyat  |  <a href="https://github.com/Ojashri-Basnyat/React-Portfolio/raw/master/src/assets/Ojashri-Basnyat_Resume.pdf" class="link">Download Resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51"> </a>
+        &copy; Ojashri Basnyat  |  <a href="https://github.com/Ojashri-Basnyat/React-Portfolio/raw/master/src/assets/Ojashri-Basnyat_Resume.pdf" class="link">Download Resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51"> |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/> </a>
         </p>
 
     </div>
